d1fe572fef1c9d943825c951a2f73ac22458ea25
Luis Antonio Montoya Morales <devlumniom@gmail.com>
2023-10-30 15:42:17 -0600

    cambios ui

--- client/src/components/pages/App.tsx
+++ client/src/components/pages/App.tsx
@@ -5,6 +5,7 @@ import { useSession } from "../../utils/hooks/hooks.ts";
 import { useEffect } from "react";
 import { Bars3Icon } from "@heroicons/react/24/outline";
 import Navigator from "../organisms/Navigator.tsx";
+import useChat from "../../utils/hooks/useChat.ts";
 
 const image = "https://www.catholicsingles.com/wp-content/uploads/2020/06/blog-header-3-768x464.png";
 const name = "Carlo Nicoli";
@@ -12,13 +13,14 @@ const position = "Overseer";
 
 function App() {
     const [session, setSession] = useSession();
+    const [state, dispatch] = useChat({});
     const navigate = useNavigate();
 
     return (
         <>
             <header
                 className={"w-full h-fit bg-blue-600 flex items-center justify-between pr-2 shadow-black shadow-sm"}>
-                <SessionInfo image={image} name={name} position={position}/>
+                <SessionInfo image={session.photo} name={session.name} position={session.role}/>
                 <Navigator/>
             </header>
             <AppNav/>
--- client/src/utils/actions/Providers.ts
+++ client/src/utils/actions/Providers.ts
@@ -4,4 +4,6 @@ const provider: AxiosInstance = axios.create( {
     baseURL: import.meta.env.VITE_SERVER_URL,
 } );
 
-export { provider };
+const chat_provider = new WebSocket("ws://"+ "127.0.0.1:8000/" + import.meta.env.VITE_CHAT_ENDPOINT);
+
+export {provider, chat_provider };
